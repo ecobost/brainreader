94487952f1f3fbe77959087fed9f96606e52b023
ecobost <emcobost@gmail.com>
2020-04-02 19:26:03 -0500

    package setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name='brainreader',
+    version='0.0.1',
+    description='Reconstruct images from recorded two-photon activity',
+    author='Erick Cobos',
+    author_email='ecobos@bcm.edu',
+    license='MIT',
+    url='https://github.com/ecobost/brainreader',
+    keywords= 'brainreader decoding two-photon',
+    packages=['brainreader'],
+    install_requires=['datajoint>=0.12', 'torch>=1.0', 'numpy'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English'
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+)
