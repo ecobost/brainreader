072e875b5755230949fe0f0f778873ebcd40090a
ecobost <emcobost@gmail.com>
2020-04-02 20:13:51 -0500

    kubernetes scripts

---
+++ kubernetes_script.py
@@ -0,0 +1,6 @@
+"""Kubernetes script"""
+#TODO: Make sure that when populating poisson loss I use std normalization and exp activatio
+
+# Training initial models
+from brainreader import train
+train.TrainedModel.populate()
