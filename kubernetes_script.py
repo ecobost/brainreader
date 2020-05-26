2054babd5c5afc26861ee27404f5cd45bb5d49ac
ecobost <emcobost@gmail.com>
2020-05-26 00:26:43 -0500

    k8 script

--- kubernetes_script.py
+++ kubernetes_script.py
@@ -134,12 +134,16 @@
 
 
 
-# from brainreader.encoding import train
-# train.TrainedModel.populate('dset_id in (1, 5)', reserve_jobs=True)
-# train.Evaluation.populate(reserve_jobs=True)
-# train.Ensemble.populate(reserve_jobs=True)
-# train.EnsembleEvaluation.populate(reserve_jobs=True)
+from brainreader.encoding import train
+train.TrainedModel.populate('dset_id in (1, 5)', reserve_jobs=True)
+train.Evaluation.populate(reserve_jobs=True)
+train.Ensemble.populate(reserve_jobs=True)
+train.EnsembleEvaluation.populate(reserve_jobs=True)
 
 # train Gabor
-from brainreader import decoding
-decoding.GaborModel.populate('dset_id in (1, 5)', reserve_jobs=True)
+# from brainreader import decoding
+# decoding.GaborModel.populate('dset_id in (1, 5)', reserve_jobs=True)
+
+# train AHP
+# from brainreader import reconstructions
+# reconstructions.AHPValEvaluation.populate(reserve_jobs=True)
