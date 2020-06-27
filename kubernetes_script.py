478b927b1bf79529283d46bbbee560a2c2327fa7
ecobost <emcobost@gmail.com>
2020-06-27 01:19:36 -0500

    k8 script

--- kubernetes_script.py
+++ kubernetes_script.py
@@ -185,11 +185,8 @@ from brainreader import decoding
 # decoding.MLPValEvaluation.populate(reserve_jobs=True)
 # decoding.DeconvModel.populate(reserve_jobs=True)
 # decoding.DeconvValEvaluation.populate(reserve_jobs=True)
-decoding.GaborModel.populate('gabor_params < 81', reserve_jobs=True)
-decoding.GaborModel.populate('gabor_params > 80 AND dset_id in (1, 4, 5)', reserve_jobs=True)
+decoding.GaborModel.populate(reserve_jobs=True)
 decoding.GaborValEvaluation.populate(reserve_jobs=True)
-decoding.GaborReconstructions.populate({'dset_id': 4}, reserve_jobs=True)
-decoding.GaborEvaluation.populate(reserve_jobs=True)
 
 #from brainreader import reconstructions
 # Use val_corr in EnsembleEvaluation to add an entry in reconstructions.BestEnsemble
